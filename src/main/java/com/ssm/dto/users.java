93780c1769f0d6b11a4eb250594524a1c29f2d27
mm <763796403@qq.com>
2017-09-18 13:40:08 +0800

    新修改的

---
+++ src/main/java/com/ssm/dto/users.java
@@ -0,0 +1,27 @@
+package com.ssm.dto;
+
+/**
+ * Created by mm on 2017/8/23.
+ */
+public class users {
+    private int id;
+    private String username;
+    private String password;
+    private String enabled;
+
+    public int getId() {return id;}
+
+    public void setId(int id) {this.id = id;}
+
+    public String getUsername() {return username;}
+
+    public void setUsername(String username) {this.username = username;}
+
+    public String getPassword() {return password;}
+
+    public void setPassword(String password) {this.password = password;}
+
+    public String getEnabled() {return enabled;}
+
+    public void setEnabled(String enabled) {this.enabled = enabled;}
+}
